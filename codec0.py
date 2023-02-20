69af8fc06fcd94cd09ba36f0f4b63fb9bfeb525a
Christos <giachoud@gapps.auth.gr>
2023-02-20 12:14:06 +0200

    Comments are done

--- codec0.py
+++ codec0.py
@@ -1,74 +1,84 @@
+"""
+3.1.4: Subband filtering
+Implementation of codec0, coder0 and decoder0 functions.
+We import frame, mp3 and nothing python files that are given by the task.
+"""
+
 import numpy as np
 from frame import *
 from mp3 import *
 from nothing import *
-# xhat, Ytot = codec0(wavin, h, M, N)
 
 def codec0(wavin, h, M, N):
     """
-    wavin:      wav file
+    wavin:      wav file to be coded and decoded
     h:          standard impulse response
     M:          number of filters
     N:          number of samples
+
+    The function returns two matrices:
+    - Ytot: has a size of [iterations*N, M]
+    - xhat: vector
     """
     H = make_mp3_analysisfb(h, M)
     G = make_mp3_synthesisfb(h, M)
     L = len(h)
-    # H: 512 x 32
 
-    # 4.a: Reading samples
+    # 3.1.4.a: Reading samples and calculating the iterations
     samples = M*N # 32X36
     iterations = int((wavin.size)/samples)
     print(f"iterations: {iterations}")
 
-    # buffer size
-    q = (N-1)*M + L
+    # buffer size given by the task: q = (N-1)*M + L
+    # Creating Ytot and xhat matrices
     Ytot = np.empty([0, M])
-    print(Ytot)
-    print(f"Shape of Ytot: {np.shape(Ytot)}")
     xhat = []
-    ytot_temp = np.empty((N,M))
 
     padding = L - M
     for i in range(iterations):
+        # We have to perform a padding action for the last iteration 
         if (i == iterations - 1):
             x_buffer = wavin[(i * samples) : (((i+1) * samples))]
             x_buffer = np.pad(x_buffer, (0, padding), 'constant')
         else: 
             x_buffer = wavin[(i * samples) : (((i+1) * samples + L - M))]
         
+        # 3.1.4.b: Computation of the frame Y
         Y = frame_sub_analysis(x_buffer, H, N)
+
+        # 3.1.4.c: Process via donothing function
         Yc = donothing(Y)
-        print(f"Shape of Yc: {np.shape(Yc)}")
+        
+        # 3.1.4.d: Collect the coded frames into Ytot matrix
         Ytot = np.append(Ytot, Yc, axis=0)
+
+        # 3.1.4.e: For each frame we perform the inverse idonothing function
         Yh = idonothing(Yc)
+
+        # 3.1.4.f: Production of xhat vector using the frame_sub_synthesis function
         xhat = np.append(xhat, frame_sub_synthesis(Yh, G))
     
-    print(f"Shape of Ytot: {np.shape(Ytot)}")
-    print(f"Shape of Yh: {np.shape(Yh)}")
-    print(f"Shape of xhat: {np.shape(xhat)}")
     return xhat, Ytot
 
-    # Coder0 Implementation
+    # 3.1.5: Coder0 Implementation. Function that implements the steps 3.1.4.a - 3.1.4.d
 def coder0(wavin, h, M, N):
 
     """
-    wavin:      wav file
+    wavin:      wav file to be coded
     h:          standard impulse response
     M:          number of filters
     N:          number of samples
+
+    The function returns the Ytot matrix
     """
     H = make_mp3_analysisfb(h, M)
     L = len(h)
-    # H: 512 x 32
 
-    # 4.a: Reading samples
+    # 4.a: Reading samples and calculating the iterations
     samples = M*N # 32X36
     iterations = int((wavin.size)/samples)
-    print(f"iterations: {iterations}")
 
-    # buffer size
-    # q = (N-1)*M + L
+    # buffer size q = (N-1)*M + L
     Ytot = np.empty((0, M))
     
     padding = L - M
@@ -76,31 +86,26 @@ def coder0(wavin, h, M, N):
         if (i == iterations - 1):
             x_buffer = wavin[(i * samples) : (((i+1) * samples))]
             x_buffer = np.pad(x_buffer, (0, padding), 'constant')
-            # print("If check")
         else: 
-            # print("Check")
             x_buffer = wavin[(i * samples) : (((i+1) * samples + L - M))]
         
-        # print(f"Size of x_buffer: {np.shape(x_buffer)}")
         Y = frame_sub_analysis(x_buffer, H, N)
         Yc = donothing(Y)
         Ytot = np.append(Ytot, Yc, axis=0)
-    print(f"Shape of Ytot1: {np.shape(Ytot)}")
-    print("Coder sucks!")
+
     return Ytot
 
-
+# 3.1.6: Decoder Implementation. Function that implement steps 3.1.4.e and 3.1.4.f
 def decoder0(Ytot, h, M, N):
     """
     Ytot:       the coder result
     h:          standard impulse response
     M:          number of filters
     N:          number of samples
+
+    The function returns the decoded vector xhat
     """
     G = make_mp3_synthesisfb(h, M)
-
-    # 4.a: Reading samples
-    samples = M*N # 32X36
 
     iterations = int(len(Ytot) / N)
     print(f"iterations: {iterations}")
@@ -108,21 +113,11 @@ def decoder0(Ytot, h, M, N):
     xhat = np.array(0)
     for i in range(iterations):
         """
-        upologismos Yc mesa apo Ytot
+        Yc calculation through Ytot
         """
-        print(f"Shape of Ytot from decoder: {np.shape(Ytot)}")
-        print(Ytot[i*N, :])
         Yh = idonothing(Ytot[i*N:(i*N + N), :])
-        print(f"Shape of Yh: {np.shape(Yh)}")
         xhat = np.append(xhat, frame_sub_synthesis(Yh, G))
-
-    print("Decoder sucks as well!")  
+ 
     return xhat
-
-# Testing
-# xhat, Ytot = codec0(np.array([0, 5, 3, 5, 3, 4]), np.array([1, 2, 3]), 32, 36)
-# print(xhat)
-# print("==============================")
-# print(Ytot)
     
     
--- test.py
+++ test.py
@@ -1,9 +1,11 @@
-# def summary(a: int, b: int) -> int:
-#     print("Hello!")
-#     return a+b
+# Multimedia systems course | ECE AUTH | Winter semester 2022
+# Simplified mp3 CODEC
+# Giachoudis Christos
+# Kostopoulos Andreas Marios
 
-# print(summary(3, 5))
-
+"""
+Make the necessary imports
+"""
 import numpy as np
 import math
 import matplotlib.pyplot as plt
@@ -13,30 +15,109 @@ from frame import *
 from mp3 import *
 import winsound
 from codec0 import *
+import wave
 
+"""
+Load h coefficients
+"""
 h_coeffs = np.load('h.npy', allow_pickle=True).tolist()
-# print(type(h_coeffs))
+# print(type(h_coeffs)) # just checking
 h_coeffs = h_coeffs['h'].reshape(-1,)
-# print(h_coeffs[4])
-# print(h_coeffs)
-# print(len(h_coeffs))
-# print(type(h_coeffs))
+# print(h_coeffs[4]) # just checking
+print("The prototype h coeffecients are:")
+print(h_coeffs)
+print("with size:")
+print(len(h_coeffs))
+# print(type(h_coeffs)) # just checking
 
+"""
+Import the sound wav file
+"""
 samplerate, data = wavfile.read('./myfile.wav')
+print(f"WAV file imported successfully!!! This file has been created with a sample rate of {samplerate} Hz.")
 
+"""
+3.1.1: Compute H and G
+"""
 fs = 44100
 h = h_coeffs # the standard h coefficients
 M = 32 # number of filters
 H = make_mp3_analysisfb(h, M) # subband filtering analysis coefficients computation
 G = make_mp3_synthesisfb(h, M) # subband filtering synthesis coefficients computation
 
-M = 32
+"""
+3.1.2: Vizualize H and G in Hz
+"""
+# Preparing the figure
+plt.figure(figsize=(10,5))
+Hs = np.transpose(H)
+
+# Fourier Transform
+fft_h = []
+for i in range(M):
+    fft_h.append(fft.rfft(Hs[i]))
+fft_h_db = 10*np.log10((np.abs(fft_h))**2)
+
+# Fixing the frequency domain
+n = 2 * fft_h_db[0].size - 1
+timestep = 1/fs
+
+freq = fft.rfftfreq(n, timestep)
+
+# Plotting
+for i in range(M):
+    plt.plot(freq, fft_h_db[i])
+
+plt.show()
+
+"""
+3.1.3: Vizualize H and G in barks
+"""
+# Typical Conversion 
+bark = 13*np.arctan(0.00076*freq) + 3.5*np.arctan((freq/7500)**2)
+
+# Traunmuller conversion (same thing, just another way to make the conversion to barks)
+# bark = ((26.81*freq) / (1960+freq)) - 0.53
+# Wang, Sekey & Gersho (not working properly)
+# bark = 6 * np.arccosh(freq/600)
+
+# Plotting
+
+# Preparing the figure
+plt.figure(figsize=(10,5))
+for i in range(M):
+    plt.plot((bark), fft_h_db[i])
+
+plt.show()
+
+"""
+3.1.4: Call codec0
+"""
 N = 36
 samples = M*N
 iterations = (data.size)/samples
-# print(iterations)
-
+print(f"{iterations} iterations where done for the calculations")
 xhat, Ytot = codec0(data, h, M, N)
 
+"""
+3.1.5: Call coder0
+"""
 Ytot_1 = coder0(data, h, M, N)
+
+"""
+3.1.6: Call decoder0
+"""
 xhat_1 = decoder0(Ytot_1, h, M, N)
+
+"""
+3.1.7a: Compare the 2 signals(sound files)
+"""
+# Original file test
+winsound.PlaySound('myfile.wav', winsound.SND_FILENAME)
+
+# Test file after applying the codec
+out = wave.open("sound.wav", "wb")
+out.setnchannels(1)
+out.setsampwidth(2) # number of bytes
+out.setframerate(44100)
+out.writeframesraw(data)
